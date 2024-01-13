330986d13ad0340d0448208bb903419cf23b4d08
aaron <aaron.young@opensky.link>
2024-01-13 15:05:42 +0800

    refine:scripts

--- deploy/00_token.js
+++ deploy/00_token.js
@@ -4,10 +4,24 @@ module.exports = async ({getNamedAccounts, deployments, network}) => {
   const {deploy} = deployments;
   const {deployer} = await getNamedAccounts();
   const totalSupply = ethers.BigNumber.from(10).pow(18).mul(config.TOTAL_SUPPLY);
+
+  let receiver
+  const tags= network.config.tags
+  if(tags.includes('test')){
+    receiver = deployer
+  } else if(tags.includes('goerli')){
+    receiver= process.env.TEST_TOKEN_RECEIVER
+  }else if(tags.includes('mainnet')){
+    receiver= process.env.PRODUCTION_TOKEN_RECEIVER
+  }
+  console.log('deploy.OSKYToken', totalSupply.toString(), receiver)
+
+  if(!receiver) throw new Error('receiver undefined')
+
   await deploy('OSKYToken', {
     from: deployer,
     args: [
-      totalSupply, deployer
+      totalSupply, receiver
     ],
     log: true,
   });
--- hardhat.config.js
+++ hardhat.config.js
@@ -13,11 +13,6 @@ require("@nomiclabs/hardhat-waffle");
 // https://github.com/motdotla/dotenv
 require('dotenv').config({silent: true});
 
-real_accounts = undefined;
-if(process.env.DEPLOYER_KEY) {
-  real_accounts = [process.env.DEPLOYER_KEY];
-}
-
 module.exports = {
   solidity: "0.8.7",
   namedAccounts: {
@@ -27,13 +22,18 @@ module.exports = {
   },
   networks: {
     hardhat: {
-      initialDate: config.UNLOCK_BEGIN,
       tags: ["test"],
     },
+    goerli: {
+      tags: ['goerli'],
+      accounts: [process.env.TEST_DEPLOYER_KEY],
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    },
     mainnet: {
+      tags: ['mainnet'],
       url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_MAINNET_KEY}`,
       chainId: 1,
-      accounts: real_accounts,
+      accounts:  [process.env.PRODUCTION_DEPLOYER_KEY],
       maxPriorityFeePerGas: 1000000000
     },
     tenderly: {
